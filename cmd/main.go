3e0d056e976e4a4d31882ba67555e61554d6e73c
jenovax86 <shruzvn@gmail.com>
2024-01-01 23:03:02 +0330

    feat: add http landing server functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,19 @@
 package main
 
-
 import (
-
+	"fmt"
+	"net/http"
 )
 
-func main(){
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "view/index.html")
+}
 
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is listening on port 4000")
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
